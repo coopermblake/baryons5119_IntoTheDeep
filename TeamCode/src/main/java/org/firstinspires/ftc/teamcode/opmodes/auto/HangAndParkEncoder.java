157db46cb80439a2e60827fe2f8b73e0e3c0c705
baryons5119ftc <baryons5119ftc@gmail.com>
2025-01-22 22:39:26 -0600

    auto opmode comments for easier to read

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/HangAndParkEncoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/HangAndParkEncoder.java
@@ -6,14 +6,20 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.core.Robot;
 
-@Autonomous(name = "10Hang and park encoder", group = "Competition Opmodes")
+@Autonomous(name = "Hang and park encoder", group = "Competition Opmodes")
 public class HangAndParkEncoder extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         // TODO: huge hack, please replace this later
+
+        //initialize stuff
         Robot robot = new Robot(hardwareMap, gamepad1, gamepad2);
         robot.initAutoDrive(telemetry);
+
+        //close gripper to hold specimen
         robot.gripper.setPosition(0.81);
         waitForStart();
+
+        //move slide to hanging position
         int lowPosition = robot.slideRot.getCurrentPosition();
         int minPosition = robot.slideExt.getCurrentPosition();
         robot.slideRot.setTargetPosition(lowPosition - 2800);
@@ -24,19 +30,26 @@ public class HangAndParkEncoder extends LinearOpMode {
         robot.slideExt.setPower(0.2);
 
         sleep(1000);
+
+        //drive to middle
         robot.autoDrive.driveStraightEncoder(0.2, 28, 0.0);
         sleep(500);
 
+        //retract and move arm down
         robot.slideExt.setTargetPosition(minPosition);
         robot.slideRot.setTargetPosition(lowPosition - 2000);
         sleep(1000);
 
+        //release specimen to hang it
         robot.gripper.setPosition(0.47);
 
+        //go right towards floor pieces
         robot.autoDrive.driveStrafeEncoder(0.4, 32, 0.0);
 
+        //move to other side of floor pieces
         robot.autoDrive.driveStraightEncoder(0.2, 28, 0.0);
-//
+
+        //line up robot with piece
         robot.autoDrive.driveStrafeEncoder(0.4, 10, 0.0);
 
 
@@ -44,10 +57,28 @@ public class HangAndParkEncoder extends LinearOpMode {
         telemetry.update();
         sleep(1000);
 
+        //turn around to push with front bumper
         robot.autoDrive.turnToHeadingEncoder(0.3, 180);
         robot.autoDrive.holdHeading(0.2, 180, 1.0);
 
+        //push piece towards human
         robot.autoDrive.driveStraightEncoder(0.4, 39, 180.0);
+
+        //slowly drive forward
+
+        //grab piece
+
+        //reverse
+
+        //turn around
+
+        //strafe left towards bars
+
+        //drive forward towards bars
+
+        //repeat hanging specimen, pushing new piece, picking up specimen, as many times as needed
+
+
 
 //        robot.slideRot.setTargetPosition(lowPosition);
 //        robot.slideExt.setTargetPosition(minPosition);
