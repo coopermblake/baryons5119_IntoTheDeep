274d5210cff87199a2b14895f9089c5c6b765adc
baryons5119ftc <baryons5119ftc@gmail.com>
2025-02-07 08:38:00 -0600

    rr auto up till hang second spec

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/RR_Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/RR_Auto.java
@@ -4,6 +4,7 @@ import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.ParallelAction;
 import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.SleepAction;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -31,15 +32,39 @@ public class RR_Auto extends LinearOpMode {
 
     @Config
     public static class Acons {//Auto Constants
-        public static double _0_x = 0;
-        public static double _0_y = -72;
-        public static double _0_theta = Math.toRadians(90);
-        public static double _1_y = -42;
-        public static double _2_wait = 0.5;
+        public static double grabPreDelay = 1;
+        public static double grabPostDelay = 1;
+        public static double grabRaiseMoveDelay = 1;
+        public static double _0_x = 9;
+        public static double _0_y = -63.5;
+        public static double _0_theta = 90;
+        public static double _1_y = -33;
+        public static double _2_x= 24;
+        public static double _2_y = -42;
+        public static double _2_t = 0;
+        public static double _2_arm_wait = 1;
+        public static double _3_x = 36;
+        public static double _3_y = -24;
+        public static double _3_t = 90;
+        public static double _4_x = 44;
+        public static double _4_y = 0;
+        public static double _4_t = 90;
+        public static double _5_h = -90;
+        public static double _6_y = -50;
+        public static double _7_x = 0;
+        public static double _7_y = -31;
+        public static double _7_h = 90;
+        public static double _7_st = 180;
+        public static double _7_et = 90;
+        public static double _8_x = _4_x;
+        public static double _8_y = _6_y;
+        public static double _8_h = 270;
+        public static double _8_st = 270;
+        public static double _8_et = 0;
 
     }
     public void runOpMode(){
-        Pose2d initialPose = new Pose2d(Acons._0_x, Acons._0_y, Acons._0_theta);
+        Pose2d initialPose = new Pose2d(Acons._0_x, Acons._0_y, Math.toRadians(Acons._0_theta));
 
         MecanumDrive mecanumDrive = new MecanumDrive(hardwareMap, initialPose);
         ViperSlide viperSlide = new ViperSlide(hardwareMap.get(DcMotor.class, "slideExt"),
@@ -48,31 +73,74 @@ public class RR_Auto extends LinearOpMode {
                                                 hardwareMap.get(Servo.class, "gripper"));
 
         TrajectoryActionBuilder startToBar = mecanumDrive.actionBuilder(initialPose)
+                .waitSeconds(0.5)
                 .setTangent(Math.toRadians(90))
                 .lineToY(Acons._1_y);
 
+        TrajectoryActionBuilder pushPieceToOZ = mecanumDrive.actionBuilder(new Pose2d (Acons._0_x, Acons._1_y, Math.toRadians(Acons._0_theta)))
+                .splineToConstantHeading(new Vector2d(Acons._2_x, Acons._2_y), Math.toRadians(Acons._2_t))
+                .splineToConstantHeading(new Vector2d(Acons._3_x, Acons._3_y), Math.toRadians(Acons._3_t))
+                .splineToConstantHeading(new Vector2d(Acons._4_x, Acons._4_y), Math.toRadians(Acons._4_t))
+                .turnTo(Math.toRadians(Acons._5_h))
+                .lineToY(Acons._6_y);
+
+        TrajectoryActionBuilder OZToBar = mecanumDrive.actionBuilder(new Pose2d(Acons._8_x, Acons._8_y, Acons._8_h))
+                .setTangent(Math.toRadians(Acons._7_st))
+                .splineToLinearHeading(new Pose2d(Acons._7_x, Acons._7_y, Math.toRadians(Acons._7_h)), Math.toRadians(Acons._7_et));
+//        TrajectoryActionBuilder BarToOZ = mecanumDrive.actionBuilder()
+//                .setTangent(Math.toRadians(Acons._8_st))
+//                .splineToLinearHeading(new Pose2d(Acons._8_x, Acons._8_y, Math.toRadians(Acons._8_h)), Math.toRadians(Acons._8_et));
 
         //opMode starts here
-        Actions.runBlocking(viperSlide.closeGripper());
 
         while(!isStopRequested() && opModeInInit()){
             telemetry.addLine("init");
-            telemetry.addData("Pose", mecanumDrive.localizer.getPose());
+            telemetry.addData("X", mecanumDrive.localizer.getPose().position.x);
+            telemetry.addData("Y", mecanumDrive.localizer.getPose().position.y);
+            telemetry.update();
+            //telemetry.addData("Heading", mecanumDrive.localizer.getPose().heading.toDouble());
+            Actions.runBlocking(viperSlide.closeGripper());
         }
 
         waitForStart();
 
         if(isStopRequested()) return;
-
+        viperSlide.reverseExt();
+        viperSlide.resetEncoder();
         Actions.runBlocking(
                     new SequentialAction(
                             //raise arm and drive to bar
                             new ParallelAction(
-                                    startToBar.build(),
+                                    viperSlide.extendToHang(),
+                                    viperSlide.rotateUp(),
+                                    startToBar.build()
+                            ),
+                            viperSlide.retractToHang(),
+                            viperSlide.openGripper(),
+
+                            new ParallelAction(
+                                    pushPieceToOZ.build(),
+                                    new SequentialAction(
+                                            new SleepAction(Acons._2_arm_wait),
+                                            viperSlide.extendToHome(),
+                                            viperSlide.rotateHorizontal()
+                                    )
+
+                            ),
+                            new SleepAction(Acons.grabPreDelay),
+                            viperSlide.extendToGrab(),
+                            viperSlide.closeGripper(),
+                            new SleepAction(Acons.grabPostDelay),
+                            new ParallelAction(
                                     viperSlide.rotateUp(),
                                     new SequentialAction(
-                                            new SleepAction(0.2),
+                                            new SleepAction(Acons.grabRaiseMoveDelay),
                                             viperSlide.extendToHang()
+                                    ),
+                                    new SequentialAction(
+                                            new SleepAction(Acons.grabRaiseMoveDelay),
+                                            OZToBar.build()
+
                                     )
                             ),
                             viperSlide.retractToHang(),
@@ -80,6 +148,12 @@ public class RR_Auto extends LinearOpMode {
                 )
 
         );
+
+        telemetry.addLine("done");
+        telemetry.addData("current", viperSlide.slideExt.getCurrentPosition());
+        telemetry.addData("min", viperSlide.extMin);
+        telemetry.update();
+        sleep(10000);
 
     }
 
